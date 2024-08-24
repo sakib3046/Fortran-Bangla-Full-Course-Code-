580851570d12dbc9e6ab3ed4d34abb780bdeea38
Md. Sakib Hasan <sakib3046@gmail.com>
2024-08-24 13:00:57 +0600

    Fortran Tutorial code

--- Chapter-4/ch-4.f95
+++ Chapter-4/ch-4.f95
@@ -1,1 +1,41 @@
 
+! program name
+!     implicit none
+!     real::a=3
+!     real::b=2
+!     real::n=2
+!     real:: sum
+!     sum=  ((a**2)**(1.0/3.0)+sqrt(a**3))/sqrt((a*(sqrt(b)))+a)
+!     print*,sum
+! end program name
+
+
+
+
+! program name
+!     implicit none
+!     real::a=3
+!     real::b=2
+!     real::n=2
+!     real:: sum
+!     sum= (1/2)*alog10((1+abs(sin(THETA*(3.1416/180))))/(1-abs(sin(THETA*(3.1416/180)))))
+!     print*,sum
+! end program name
+
+
+
+! program name
+!     implicit none
+!     real::x=5.4
+! 33 print*,"this is 33"
+!     if (x.gt.5.5) then
+!         goto 33
+!     else
+!         print*,"Nothing to do"
+!     end if
+! end program name
+
+program name
+        implicit none
+        real::seven
+    end program name
